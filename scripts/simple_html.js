a3b9acaf89f5ea4bc42934b804ac27b94828142c
Saulius Baltramonaitis <msnfan234@gmail.com>
2019-02-19 12:51:24 +0000

    add random text colour functionality

--- scripts/simple_html.js
+++ scripts/simple_html.js
@@ -1,7 +1,20 @@
+//Methods to show and remove current date respectively
 function getDate() {
-    document.getElementById("currDate").innerHTML = Date();
+    document.getElementById('currDate').innerHTML = Date();
 }
 
 function removeDate() {
-    document.getElementById("currDate").innerHTML = "";
+	document.getElementById('currDate').innerHTML = "";
+}
+
+//Random colour generator for 'Lorem ipsum text'
+var colours = ['green', 'blue', 'yellow', 'grey', 'red', 'orange'];
+var arrLength = colours.length;
+var text = document.getElementById('randColour');
+
+function colourText() {
+	var rand = Math.floor(Math.random() * arrLength);
+	console.log(colours[rand]);
+	
+	document.getElementById('randColour').style="color: "+colours[rand];
 }
